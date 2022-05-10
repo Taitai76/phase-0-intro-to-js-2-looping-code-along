cea596d7940a251c12db46a754bf6002a1806a64
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-10 01:04:25 -0400

    commit tree af2b8c57650ba0e48a56d18e7c0a93d48f35d6be

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names=['name1','name 2','name3'];
+
+function writeCards(names, string){
+    const cards=[];
+    for (let i=0; i< names.length; i++){
+        cards[i]=`Thank you, ${names[i]}, for the wonderful ${string} gift!`;
+
+    }
+    return cards;
+}
+function countDown(x){
+    while (x>=0){
+        console.log(x);
+        x -= 1;
+        
+    }
+}
